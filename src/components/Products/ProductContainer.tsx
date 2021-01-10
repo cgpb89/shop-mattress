a5c116d154fa0b9778917873daef8cbd7ca29d46
Carlos Pereira <carlosp@thehangar.cr>
2021-01-09 18:02:41 -0600

    Shipping Cart

---
+++ src/components/Products/ProductContainer.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Container, Row } from "reactstrap";
+import Mattress from '../../model/Mattress';
+import MattressType from '../../model/MattressType';
+import SelectProduct from './SelectProduct';
+
+interface ProductContainerProps {
+    productList: MattressType[],
+    selectedProduct: (value: Mattress) => void,
+    productSelected: Mattress,
+    addItemToCart: (value: Mattress) => void,
+}
+
+class ProductContainer extends React.Component<ProductContainerProps, any> {
+
+    public getImage = (image: string) => {
+        return `../images/${image}`;
+    }
+
+    render() {
+        const { productList, selectedProduct, productSelected,addItemToCart } = this.props;
+
+        return (
+            <div>
+                <Container className="product-container">
+                    <Row className="product">
+                        <div className="col-xs-12 col-sm-12 col-md-8 col-lg-8 image-container">
+                             <img src={this.getImage(productSelected.get_ImageFileName())} alt={productSelected.get_Name()}/>
+                        </div>
+                        <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4 description-container">
+                            <h3>Choose Your Mattress</h3>
+                            <SelectProduct
+                                productSelected={productSelected}
+                                selectedProduct={selectedProduct}
+                                productList={productList}
+                                addItemToCart={addItemToCart}
+                            />
+                        </div>
+                    </Row>
+                </Container>
+            </div>
+        )
+    }
+}
+
+export default ProductContainer;
